331b44d3b978fac8e80368f02c1cd88d519c372c
ChamanGurjar <chamangujjar162@gmail.com>
2020-11-05 10:06:06 +0530

    State changed on click of button.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,23 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // NOTE :- Don't directly change the state like :- this.state.persons[0].name = 'Changed Name'
+    this.setState({
+      persons: [
+        {name: 'Chaman Singh Gurjar', age: 30},
+        {name: 'Abhijeet Singh', age: 2.11},
+        {name: 'Ajay', age: 21}
+      ]
+    })
+  }
+
   render() {
     return(
       <div className="App">
         <h1>Hi, My name is Chaman Gurjar.</h1>
-        <p1>This is my official website.</p1>
+        <p>This is my official website.</p>
+        <button onClick={this.switchNameHandler}> Change Name </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
