945a6a3965dcd216831e22f7f88b9ee45d0b0097
ChamanGurjar <chamangujjar162@gmail.com>
2020-12-25 16:31:37 +0530

    Success and Error Buttons Added and Success and Error Toast shown on the screen.

--- practice-one/src/components/Candidate.js
+++ practice-one/src/components/Candidate.js
@@ -1,11 +1,32 @@
-
+import { Button } from 'reactstrap'
+import { ToastContainer, toast } from 'react-toastify'
 
 function Candidate({ firstName, lastName }) {
+
+    const sucessBtnHandler = () => {
+        toast.success('Hi User...', {
+            position: "top-center"
+        })
+    }
+
+    const failureBtnHandler = () => {
+        toast.error('Hi User...', {
+            position: "top-center"
+        })
+    }
+
     return (
 
-        <div style={{ background: "yellow", padding: 10 }}>
+        <div style={{ background: "orange", padding: 10 }}>
             <p><h3>{firstName} </h3><h4>{lastName}</h4></p>
-
+            <Button color="primary" onClick={sucessBtnHandler}>
+                Hi {firstName}, Click Me to check the Sucess toast message
+            </Button>
+            <Button color="primary" onClick={failureBtnHandler}>
+                Hi {firstName}, Click Me to check the Failure toast message
+            </Button>
+            <ToastContainer />
+            <hr />
         </div>
     )
 }
